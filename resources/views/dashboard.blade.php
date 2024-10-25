4ae4a57bfe32073a4aeb6c7633a5665e2815a4df
Mauricio Monari <mmonari@uh.edu>
2024-10-25 00:54:29 -0300

    HEY-9: ask question form wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,22 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            
+                <form method="POST" action="{{ route('question.store') }}">
+                    @csrf
+                    <div class="mb-4">                    
+                        <label for="question" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your question</label>
+                        <textarea id="question" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+                    </div>
+                    <button type="submit" class="px-5 py-2.5 mb-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Ask the question</button>
+                </form>
+            
         </div>
     </div>
 </x-app-layout>
