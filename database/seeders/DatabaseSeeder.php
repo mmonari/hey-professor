272f3a61d70bc060b4a36da532f9569cdb35b7d1
Mauricio Monari <mmonari@uh.edu>
2024-10-24 23:06:58 -0300

    HEY-9: created seed and factory question model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\{Question, User};
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name'  => 'Test User',
+            'name'  => 'Bob',
             'email' => 'test@example.com',
         ]);
+
+        Question::factory(10)->create();
     }
 }
