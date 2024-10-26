8bb52a74b4784d2b2e6eedb0621d5c461c928e7d
Mauricio Monari <mmonari@uh.edu>
2024-10-26 18:55:52 -0300

    HEY-9: form to ask question

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,9 +12,14 @@
                     @csrf
                     <div class="mb-4">                    
                         <label for="question" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your question</label>
-                        <textarea id="question" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+                        <textarea name="question" id="question" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment...">{{ old('question') }}</textarea>
+                        @error('question')
+                            <p class="mt-1 text-xs text-red-500">{{ $message }}</p>
+                        @enderror
                     </div>
+                    <button type="button" class="px-5 py-2.5 mb-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 me-2 focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Reset</button>
                     <button type="submit" class="px-5 py-2.5 mb-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Ask the question</button>
+                    
                 </form>
             
         </div>
