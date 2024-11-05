5a8bae30da26bfa2d71e2446b628cd0882f95bb9
Mauricio Monari <mmonari@uh.edu>
2024-11-05 12:52:12 -0300

    HEY-24: frontend search at dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,14 +10,26 @@
 
     <x-container>
         
-        {{-- List of questions to be voted --}}
-       
+      
         <div class="font-medium uppercase dark:text-gray-300">{{ __('Vote on questions') }}</div>
 
+        <form method="get" action="{{ route('dashboard') }}" class="flex flex-row gap-x-2 justify-end items-center">
+            <x-text-input name="search" :value="request('search')" placeholder="{{ __('Search') }}" />
+            <x-form.btn-primary type="submit" :label="__('Search')" />
+        </form>
+        
         <div class="mt-2 space-y-3">
-            @foreach($questions as $item)
-                <x-question.card :question="$item" />
-            @endforeach
+            @if($questions->isEmpty())
+                <hr class="my-4 border-gray-500 border-dashed">
+                <div class="flex flex-col items-center mt-4">
+                    <x-typo.subtitle>{{ __('No questions found!') }}</x-typo.subtitle>
+                    <a href="{{ route('dashboard') }}" class="ml-2 text-blue-500">{{ __('Browse all questions') }}</a>
+                </div>
+            @else
+                @foreach($questions as $item)
+                    <x-question.card :question="$item" />
+                @endforeach
+            @endif
         </div>
        
         <div class="mt-4">
